f82e4bb1a86eb8fd3a26ff51e5d044ef1a5ed24b
loockeeer <lulu.r1313@gmail.com>
2021-06-20 23:39:16 +0200

    chore: update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("com.github.gmazzo.buildconfig") version "3.0.0"
 }
@@ -13,16 +14,20 @@ group = "fr.westerosrp"
 version = "1.0.0"
 
 repositories {
-    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
-    maven(url = "https://oss.sonatype.org/content/repositories/central")
+    mavenLocal()
+    google()
+    jcenter()
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
 }
 
 val minecraft_version: String by project
 
 dependencies {
-    // PaperMC Dependency
     compileOnly("org.spigotmc", "spigot-api", "$minecraft_version-R0.1-SNAPSHOT")
+    compileOnly("net.luckperms:api:5.3")
 }
 
 buildConfig {
