7ae8da4357540affe9276627f2c2eaacd61883ab
loockeeer <lulu.r1313@gmail.com>
2021-06-29 21:22:12 +0200

    feat: add swamp sounds

--- src/main/kotlin/fr/westerosrp/WesterosRP.kt
+++ src/main/kotlin/fr/westerosrp/WesterosRP.kt
@@ -2,13 +2,16 @@ package fr.westerosrp
 
 import fr.westerosrp.command.*
 import fr.westerosrp.game.Month
-import fr.westerosrp.game.Scoreboard
+import fr.westerosrp.utils.Scoreboard
 import fr.westerosrp.game.Team
 import fr.westerosrp.game.Territory
 import fr.westerosrp.listeners.*
 import net.luckperms.api.LuckPerms
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.Sound
+import org.bukkit.SoundCategory
+import org.bukkit.block.Biome
 import org.bukkit.plugin.java.JavaPlugin
 
 class WesterosRP : JavaPlugin() {
@@ -81,6 +84,20 @@ class WesterosRP : JavaPlugin() {
 				updateTabList(it)
 			}
 		}, 0L, 20L)
+
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, {
+			Bukkit.getOnlinePlayers().forEach {
+				if(it.world.getBiome(it.location.x.toInt(), it.location.y.toInt(), it.location.z.toInt()) == Biome.SWAMP) {
+					when ((1..6).random()) {
+						1 -> it.playSound(it.location, Sound.AMBIENT_SOUL_SAND_VALLEY_MOOD, SoundCategory.MASTER, 100.0f, 1.0f)
+						2 -> it.playSound(it.location, Sound.AMBIENT_SOUL_SAND_VALLEY_ADDITIONS, SoundCategory.MASTER, 100.0f, 1.0f)
+						3 -> it.playSound(it.location, Sound.PARTICLE_SOUL_ESCAPE, SoundCategory.MASTER, 100.0f, 1.0f)
+						4 -> it.playSound(it.location, Sound.AMBIENT_UNDERWATER_LOOP_ADDITIONS_ULTRA_RARE, SoundCategory.MASTER, 100.0f, 1.0f)
+						5 -> it.playSound(it.location, Sound.ENTITY_VEX_CHARGE, SoundCategory.MASTER, 100.0f, 1.0f)
+					}
+				}
+			}
+		}, 0L, 20L*5)
 
 		if (running) {
 			scheduleMonthRoll(this)
