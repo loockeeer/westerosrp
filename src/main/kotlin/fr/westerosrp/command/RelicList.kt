3a124548fa8e2e5e041e0a6a2819858ebd406616
loockeeer <lulu.r1313@gmail.com>
2021-06-30 20:21:33 +0200

    feat: make reliclist command be open to everybody

--- src/main/kotlin/fr/westerosrp/command/RelicList.kt
+++ src/main/kotlin/fr/westerosrp/command/RelicList.kt
@@ -11,17 +11,19 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 class RelicList : CommandExecutor {
+	companion object {
+		val inventoryTitle = "${ChatColor.GRAY}${ChatColor.BOLD}> ${ChatColor.GOLD}Liste des reliques"
+	}
 	override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 		if (sender !is Player) {
 			sender.sendErrorMessage("Vous devez être un joueur pour faire cela !")
 			return true
 		}
 
-		val listInventory = Bukkit.createInventory(null, 54)
+		val listInventory = Bukkit.createInventory(null, 54, inventoryTitle)
 		Relic.values().map {
-			listInventory.addItem(it.generateItemStack())
+			listInventory.addItem(it.generateWithID())
 		}
-
 		sender.openInventory(listInventory)
 		sender.sendInfoMessage("Ouverture du menu des reliques")
 		return true
